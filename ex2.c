f64fd5ddc03171ea615fdbfe63595f8f9ed408b0
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-09-28 19:21:24 -0700

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int dola,vnd,tygia;
+	tygia =24000;
+	scanf("%d",&dola);
+	vnd= dola*tygia;
+	printf("doi ra la %d vnd",vnd);
+	return 0;
+}
