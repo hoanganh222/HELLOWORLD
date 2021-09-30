d73bb523f1f4fc8ffc534c93a77ad63432d164a3
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-09-30 08:24:01 -0700

    Create baitap_02.c

---
+++ baitap_02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+ main(){
+ 	int num = 10;
+ 	char ch = 'A';
+ 	float f = 25.3;
+ 	double d = 25.3;
+ 	printf("num = %d\n",d);
+ 	printf("ch = %d\n",ch);
+ 	printf("f = %.2f\n",f);
+ 	printf("d = %f\n",d);
+	getch();
+	return 0;
+}
