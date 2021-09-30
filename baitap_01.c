4b543de7b20c5755712316e90b2ee28ed0203298
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-09-30 08:24:23 -0700

    Create baitap_01.c

---
+++ baitap_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	return 0;
+	* Class : T1104L;
+	*RolNo : B001;
+	*Full Name : Nguyen Van A;
+	*Email : anv@fpt.aptecj.ac.vn;
+	Phone : 0912345678;
+}
