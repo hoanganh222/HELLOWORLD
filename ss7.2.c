44783aa6e4460dc6abeaeacf1d3e939e21b74196
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-10-03 08:58:12 -0700

    Create ss7.2.c

---
+++ ss7.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b;
+	printf("nhap a =");
+	scanf("%d",&a);
+	printf("nhap b");
+	scanf("%d",&b);
+	if (a*b>1000)
+	printf("lon hon");
+	else
+	printf("= hoac nho hon");
+}
