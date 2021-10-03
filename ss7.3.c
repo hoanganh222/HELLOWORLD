50123fadf1d1985945698b5bf43683e227ba468b
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-10-03 08:58:17 -0700

    Create ss7.3.c

---
+++ ss7.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b,c;
+	printf("so thu nhat la");
+	scanf("%d",&a);
+	printf("so thu hai la");
+	scanf("%d",&b);
+	printf("so can so sanh la");
+	scanf("%d",&c);
+	if (a-b==c)
+	printf("hieu = so da nhap");
+	else
+	printf("hieu khong = so da nhap");
+	
+}
