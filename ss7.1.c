c98a97ccdebf875f9abf57d166e69bcb4ffcab1c
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-10-03 08:58:08 -0700

    Create ss7.1.c

---
+++ ss7.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a,b;
+    printf("Nhap so a: "); 
+    scanf("%d",&a);
+    printf("nhap so b: ");
+    scanf("%d",&b);
+    printf("nhap so");
+    if (a%b== 0)
+    printf("nhap so");
+	else  
+    printf("khong chia het");
+}
