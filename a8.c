69fc9502dcc0859b5f7df84260faace8650d03d7
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-10-03 08:58:04 -0700

    Create a8.c

---
+++ a8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	return 0;
+	int basic;
+    printf("\n Please enter your basic:" );
+    scanf("%d",&basic);
+    switch (basic)
+{
+        case 200 : printf("\n Bonus is dollar %d\n", 50);
+        break;
+}
