f90de257c9f6105117c4b44a841d8d63fb5c9d11
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-10-03 08:58:33 -0700

    Create ss8.1.c

---
+++ ss8.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x,y;
+	x=100;
+	y=300;
+	if (x<2000||x>3000)
+	printf("%d",x);
+	if (y>100&&y<300)
+	printf("%d",y);
+	
+	
+	
+}
