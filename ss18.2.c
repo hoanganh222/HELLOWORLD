87784439c2d64831c5f3b23e824c542a081a0c36
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-10-19 11:07:16 +0700

    Create ss18.2.c

---
+++ ss18.2.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	int i;
+	char names[5] [20];
+	void uppername(char name_arr[]);
+	for (i=0;i<5;i++)
+	{
+		printf("enter string %d:",i+1);
+		scanf("%s", names[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		uppername(names[i]);
+		printf("\nNew string %d: %s", i+1,names[i]);
+	}
+	return 0;
+}
+
+void uppername(char name_arr[])
+{
+	int x;
+	for(x=0;name_arr[x]  !='\0'; x++)
+	{
+		if(name_arr[x]>=97 && name_arr[x]<=122)
+		   name_arr[x]=name_arr[x]-32;
+	}
+}
+
