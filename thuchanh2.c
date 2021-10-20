324a24d4bef80f9817851075043403547e95129e
hoanganh222 <91514975+hoanganh222@users.noreply.github.com>
2021-10-20 10:53:23 +0700

    Update thuchanh2.c

--- thuchanh2.c
+++ thuchanh2.c
@@ -3,21 +3,70 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(){
-	int numbers[10];
-	int i;
+	int a,b;
+int sum(a,b);
+int sub(a,b);
+int mul(a,b);
+float chia(a,b);
+ main() {
+	int chon;
+	char nhapdiem;
+	int kq;
+	do
+	{
+	printf("nhap so 1:\n");
+	scanf("%d",&a);
+	printf("nhap so 2:\n");
+	scanf("%d",&b);
+	printf("ban muon thuc hien phep tinh nao\n");
+	printf("1 cong\n");
+	printf("2 tru\n");
+	printf("3 nhan\n");
+	printf("4 chia\n");
+	printf("hay chon phep tinh muon thuc hien\n");
+	scanf("%d",&chon);
+	switch(chon)
+	 {
+	 	case 1:
+	 		sum(a,b);
+	 		break;
+	 	case 2:
+	 		sub(a,b);
+	 		break;
+	 	case 3:
+	 		mul(a,b);
+	 		break;
+	 	case 4:
+	 		chia(a,b);
+	 		break;
+	}
+	printf("ban co muon tiep tuc thuc hien cac phep tinh\n");
+	printf(" y de tiep tuc");
+	printf(" n de ket thuc");
+	fflush(stdin);
+	scanf("%c",&nhapdiem);
+	}while(nhapdiem!='n');
+	{
+		printf("end\n");
+	}
 	
-	for(i = 0; i < 10; i++)
-	{
-		printf("Nhap vao so nguyen thu %d: ", i+1);
-		scanf("%d", &numbers[i]);
-	}
-	printf("\nResult: ");
-	for(i = 9; i >= 0; i--)
-	{
-		printf(" %d ", numbers[i]);
-	}
+	
 	return 0;
 }
+int sum(int a, int b)
+{	printf("sum = %d",a+b);
 
+}
+int sub(int a,int b)
+{
+	printf("sub = %d",a-b);
+}
+int mul( int a,int b)
+{
+	printf("mul = %d",a*b);
+}
+float chia(int a ,int b)
+{
+	printf("div = %f",(float)a/b);
+}
 
